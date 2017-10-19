efb701d6631bd2944adf19112f3960f45dcde63d
JAESEUNG\JaeSeung <banglebangle22@gmail.com>
2017-10-19 15:46:33 +0900

    해당 기능별 컨트롤러 생성

---
+++ kobook/src/kobook/controller/BookController.java
@@ -0,0 +1,66 @@
+package kobook.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kobook.action.Action;
+import kobook.action.ActionForward;
+
+public class BookController {
+	public void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ActionForward forward = null;
+		Action action = null;
+
+		
+		//	밑의 줄이 출력되면 해당 controller로 잘 진입한것!
+		System.out.println("==========bookController==========");
+		// /kobook/book/insertForm.do
+		String requestURI = request.getRequestURI();
+		System.out.println("requestURI: " + requestURI);
+
+		// /kobook
+		String contextPath = request.getContextPath();
+		System.out.println("contextPath: " + contextPath);
+
+		// book/insertForm.do
+		String command1 = requestURI.substring(contextPath.length() + 1);
+		System.out.println("command1: " + command1);
+
+		// insertForm.do
+		String command2 = command1.substring(command1.indexOf("/") + 1);
+		System.out.println("command2: " + command2);
+
+		
+
+		// 여기서 각각의 Action으로 보낸다.
+		// 주석 풀고 각자 연결시킨 곳으로 
+		if (command2.equals("bookDetail.do")) {
+			
+			// action 객체 생성
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 
+		
+		
+		
+		// 	forward를 해체하는 과정 
+		if(forward != null){
+			if(forward.isRedirect()){	// rediredct로 페이지 이동
+				response.sendRedirect(forward.getPath());
+			} else {	//dispatcher로 페이지 이동
+				RequestDispatcher dispatcher = 
+						request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+}
