7a453c7649eeb719a71eb5d9f8386d3014ae170f
JO <JO@DESKTOP-2EIM6B2>
2017-10-25 17:21:35 +0900

    [지호] 관리자 서비스 수정 5:21

--- kobook/src/kobook/admin/service/AdminService.java
+++ kobook/src/kobook/admin/service/AdminService.java
@@ -1,20 +1,21 @@
 package kobook.admin.service;
 
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import kobook.admin.dao.PersonDAO;
 import kobook.admin.dao.BoardDAO;
+import kobook.admin.dao.PersonDAO;
 import kobook.admin.domain.Board;
 import kobook.admin.domain.ListModel;
-import kobook.admin.domain.PersonModel;
 import kobook.admin.domain.Search;
 import kobook.admin.domain.Person;
+import kobook.admin.domain.PersonModel;
 
 public class AdminService {
-	
+
 	private static AdminService service = new AdminService();
 	private static PersonDAO pdao;
 	private static BoardDAO bdao;
@@ -26,37 +27,34 @@ public class AdminService {
 		return service;
 	}
 	
-	public int searchP_idService(String person_email){
-		return pdao.searchP_id(person_email);
-	}
-	
 	public PersonModel listPersonService(HttpServletRequest request, int requestPage){
 
 		Search search = new Search();
 		HttpSession session = request.getSession();
 		
-		//���ο� �˻� �õ�(temp���� �Ѿ���°�)
+		//새로운 검색 시도(temp값이 넘어오는것)
 		if(request.getParameter("temp") != null || request.getParameter("pageNum")==null){
 			session.removeAttribute("search");
 		}
 		
-		//�˻���
+		//검색시
 		if(request.getParameterValues("area") != null){
 			search.setArea(request.getParameterValues("area"));
 			search.setSearchKey("%"+request.getParameter("searchKey")+"%");
 			session.setAttribute("search", search);
+			
 		}else if((Search)session.getAttribute("search") != null){
 			search = (Search)session.getAttribute("search");
-		}
+		} 
 		
-		//�������� �۰���, �ѱ۰���, ����������, ����������, ������������, ����������
+		//페이지당 글갯수, 총글갯수, 총페이지수, 시작페이지, 마지막페이지, 현재페이지
 		int totalCount = pdao.countPerson(search);
 		int totalPageCount = totalCount/PAGE_SIZE;
 		if(totalCount%PAGE_SIZE > 0){
 			totalPageCount++;
 		}
 		
-		int startPage = requestPage - (requestPage - 1)%5;	//start page ����� ����
+		int startPage = requestPage - (requestPage - 1)%5;	//start page 만드는 공식
 		int endPage = startPage + 4;
 		if(endPage > totalPageCount){
 			endPage = totalPageCount;
@@ -65,10 +63,9 @@ public class AdminService {
 		int startRow = (requestPage-1) * PAGE_SIZE;
 
 		List<Person> list = pdao.listPerson(startRow, search);
+		return new PersonModel (list, requestPage, totalPageCount, startPage, endPage);
 		
-		return new PersonModel(list, requestPage, totalPageCount, startPage, endPage);
-		
-
+	/*	return udao.listUser(search);*/
 	}
 
 	public Person detailPersonService(int person_id){
@@ -91,9 +88,9 @@ public class AdminService {
 		return pdao.blackPerson();
 	}
 	
-	//--------------------공지사항-------------------------		
+//--------------------공지사항-------------------------	
 	
-	//�������� �۾��� 
+	//공지사항 글쓰기 
 	public int insertBoardService(Board board){
 		board.setBoard_id(bdao.selectB_id() + 1);
 		return bdao.insertBoard(board);
@@ -103,9 +100,9 @@ public class AdminService {
 		return pdao.selectPerson(person_id);
 	}
 	
-	//�������� �ۺ����ֱ�
+	//공지사항 글보여주기
 	public ListModel listBoardService(HttpServletRequest request, int requestPage){
-		
+
 		Search search = new Search();
 		HttpSession session = request.getSession();
 		
@@ -142,7 +139,6 @@ public class AdminService {
 		return new ListModel (list, requestPage, totalPageCount, startPage, endPage);
 		/*return daoo.listBoard();*/
 	}
-
 	
 	//공지사항 글선택
 	public Board selectBoardService(int board_id, boolean bool){
@@ -167,5 +163,6 @@ public class AdminService {
 	public int updateNoti(int board_id){
 		return bdao.updateNoti(board_id);
 	}
-
-}
+	
+	
+}	
